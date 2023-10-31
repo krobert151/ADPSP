6a285d502391cf3a54218a7a85594babb3a33c00
Roberto <robertorebolledo151@gmail.com>
2023-10-31 12:53:51 +0100

    create porject

---
+++ connect2database/src/main/java/com/rebolledonaharro/connect2database/Connect2databaseApplication.java
@@ -0,0 +1,13 @@
+package com.rebolledonaharro.connect2database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Connect2databaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Connect2databaseApplication.class, args);
+	}
+
+}
