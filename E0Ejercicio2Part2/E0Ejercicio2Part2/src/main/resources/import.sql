ba1eeacda17b1bf3f0a92ad7ed2aea5b4861aeb8
Roberto <robertorebolledo151@gmail.com>
2023-11-03 12:37:14 +0100

    pal git

---
+++ E0Ejercicio2Part2/E0Ejercicio2Part2/src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO Profesor (nombre,email,puntuacion) VALUES ('Manolo','manolo@gmail.com',23);
+INSERT INTO Profesor (nombre,email,puntuacion) VALUES ('Miguel','manolo@gmail.com',34);
+INSERT INTO Profesor (nombre,email,puntuacion) VALUES ('Luismi','manolo@gmail.com',54);
+INSERT INTO Profesor (nombre,email,puntuacion) VALUES ('Manolo2','manolo@gmail.com',65);
+INSERT INTO Profesor (nombre,email,puntuacion) VALUES ('Manolo3','manolo@gmail.com',34);
+INSERT INTO Profesor (nombre,email,puntuacion) VALUES ('Manol4','manolo@gmail.com',54);
+INSERT INTO Profesor (nombre,email,puntuacion) VALUES ('Manolo5','manolo@gmail.com',26);
