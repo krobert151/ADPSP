dce96d45e520361e5c3cf0065f83634b71858c2f
Roberto Rebolledo <robertorebolledo151@gmail.com>
2023-11-04 15:06:17 +0100

    completar estos campos

---
+++ Joined/src/test/java/com/rebolledoNaharro/Joined/JoinedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rebolledoNaharro.Joined;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoinedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ TablePerClass/src/test/java/com/rebolledoNaharro/TablePerClass/TablePerClassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rebolledoNaharro.TablePerClass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TablePerClassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
